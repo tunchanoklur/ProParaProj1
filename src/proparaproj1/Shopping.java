104f5acc495fe980b2a015880e03d30629e1e9ea
DESKTOP-IAR2C23\Tunchanok <kikpanda@gmail.com>
2017-10-12 16:21:18 +0700

    test

--- src/proparaproj1/Shopping.java
+++ src/proparaproj1/Shopping.java
@@ -5,5 +5,6 @@ import java.io.*;
 public class Shopping {
     public static void main(String[] args) {
        System.out.printf("Hello\n");
+       System.out.printf("dwdjbknwknwo\n");
     }  
 }
